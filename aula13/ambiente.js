253913843059c8fdfb908860ed8b4e167613efae
Ronei.Ferreira <rifla2016@hotmail.com>
2026-02-26 19:28:08 -0300

    estrutura de repetição do

---
+++ aula13/ambiente.js
@@ -0,0 +1,41 @@
+/* estrutura de repetição com teste lógico no final*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 12)
+
+
+
+/* estrutura de repetição com teste lógico no inicio*/
+
+/*var c = 1
+while (c <= 30) {  /*testa primeiro*/
+    /*console.log(`Passo ${c}`)  /*execulta depois*/
+   /* c++
+}/*
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+
+/*AS 3 ESTRUTURAS ACIMA DÃO O MESMO RESULTADO; A MAIS USADA É A DO MEIO
+*/
+
+/* estrutura de repetição com teste lógico no inicio*/
+/*var c = 1
+while (c <= 30) {  /*testa primeiro*/
+    /*console.log(`Passo ${c}`)  /*execulta depois*/
+   /* c++
+}/*
+
+
