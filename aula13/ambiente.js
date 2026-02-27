5242ebf945d99a7f4010f531e873f938e33684a8
Ronei.Ferreira <rifla2016@hotmail.com>
2026-02-27 16:49:23 -0300

    ex16-16 java script

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -7,34 +7,5 @@ do {
 
 
 
-/* estrutura de repetição com teste lógico no inicio 
-
-var c = 1
-while (c <= 30) {       testa primeiro
-    
-console.log(`Passo ${c}`)   execulta depois
-   c++
-} */
 
 
-
-
-/*console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-*/
-
-
-/*AS 3 ESTRUTURAS ACIMA DÃO O MESMO RESULTADO; A MAIS USADA É A DO MEIO
-*/
-
-/* estrutura de repetição com teste lógico no inicio*/
-/*var c = 1
-while (c <= 30) {    testa primeiro
-    console.log(`Passo ${c}`)  execulta depois
-    c++
-} */
