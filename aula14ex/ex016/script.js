62d1981b240cc3090ec6efec4e1c192e35c605a0
Ronei.Ferreira <rifla2016@hotmail.com>
2026-02-28 12:55:46 -0300

    ex16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,12 @@
-alert('Ola')
+function contar() { 
+    let ini = document.getElementById('txti') 
+    let fim = document.getElementById('txtf') 
+    let passo = document.getElementById('txtp') 
+    let re
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { 
+        window.alert('[ERRO] Faltam Dados!') 
+    } else { 
+        alert('Tudo Ok!') 
+    } 
+}
