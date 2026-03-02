f839a5d06370fb92472dadad612c3493eb8d8de3
Ronei.Ferreira <rifla2016@hotmail.com>
2026-03-02 12:07:33 -0300

    atualizado super contador

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,9 +6,9 @@ function contar() {
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { 
         window.alert('[ERRO] Faltam Dados!') 
+        res.innerHTML = ' Impossivel contar!'
     } else { 
-        res.innerHTML = 'Contando:<br>'
-
+        res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
