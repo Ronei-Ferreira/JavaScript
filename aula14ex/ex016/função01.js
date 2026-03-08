43974b27b5032ceed90f0c62fd97797c71e2231a
Ronei.Ferreira <rifla2016@hotmail.com>
2026-03-07 21:30:03 -0300

    pratica funções

---
+++ aula14ex/ex016/função01.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n%2 ==0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar'
+    }
+}
+//se não chamar ele não aparece
+console.log(parimpar(223)) //jogar dentro de uma var aparece 
